e3071af890c0f73e435e6093092d4af8ea3a0ae1
MorettiAndrea <andrea.moretti1704@gmail.com>
2025-05-08 16:48:28 +0200

    first commit

---
+++ hobbies.js
@@ -0,0 +1,9 @@
+const personHobbies = () => {
+  const firstHobby = "Crossfit";
+  const secondHobby = "Gaming";
+  const thirdHobby = "Music";
+  return { firstHobby, secondHobby, thirdHobby };
+};
+console.log(personHobbies());
+
+module.export = { personHobbies };
---
+++ names.js
@@ -0,0 +1,9 @@
+const personData = () => {
+  const firstName = "Andrea";
+  const lastName = " Moretti";
+  return { firstName, lastName };
+};
+
+console.log(personData());
+
+module.export = { personData };
